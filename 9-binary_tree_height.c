4f34d5e331ab9d0097e19228b88a8eac0a9325f4
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-11 12:45:22 -0300

    hotfix task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	else
 	{
-		int leftHeight = binary_tree_height(tree->left);
-		int rightHeight = binary_tree_height(tree->right);
+		leftHeight = binary_tree_height(tree->left);
+		rightHeight = binary_tree_height(tree->right);
 	if (leftHeight >= rightHeight)
 		return (leftHeight + 1);
 	else
