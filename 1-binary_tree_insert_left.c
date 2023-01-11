c390a8d5cc0189b52336c685ef561f5a1e8dabf9
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-11 10:38:53 -0300

    border case considered

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	Newnode = mycreate_node(value);
 	if (Newnode == NULL)
 		return (NULL);
+	if (parent)
+		return (NULL);
 
 	Newnode->parent = parent;
 
