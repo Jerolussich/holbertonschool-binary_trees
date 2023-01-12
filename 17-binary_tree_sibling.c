690da46f9f822a6b4f56fec28bd23b5d351f03b3
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-12 11:16:42 -0300

    task 17 border case added

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -16,9 +16,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	n = node->n;
 	my_node = node->parent;
 
+	if (!my_node->left)
+		return (NULL);
 	if (my_node->left->n == n)
 		my_node = my_node->right;
-	else
+	else if (my_node->left->n == n)
 		my_node = my_node->left;
 	return (my_node);
 }
