162767f2f500f9f481d4928f03439d52068bca73
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-11 21:13:58 -0300

    task 16 hotfix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (NULL);
+		return (0);
 	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
 		return (1);
 	return (0);
