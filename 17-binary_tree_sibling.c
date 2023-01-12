3190deecf223448fc7a54f4ef268204588928f57
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-12 11:18:40 -0300

    hotfix added

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	if (my_node->left->n == n)
 		my_node = my_node->right;
-	else if (my_node->left->n == n)
+	else
 		my_node = my_node->left;
 	return (my_node);
 }
